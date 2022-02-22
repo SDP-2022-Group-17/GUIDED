5bb371d6be8d87ecd9d06895acad5f3f62eae748
Marcel Marais <s1970051@jingxi.inf.ed.ac.uk>
2022-02-22 15:12:40 +0000

    Added new nav stuff

--- UI/navigation.py
+++ UI/navigation.py
@@ -2,20 +2,39 @@
 import rospy
 from geometry_msgs.msg import Twist
 import roslaunch
+import actionlib
 
-def talker():
-    pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    # while not rospy.is_shutdown():
-    #    roslaunch simple_navigation_goals movebase_seq.launch
-    uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
-    roslaunch.configure_logging(uuid)
-    path = "/afs/inf.ed.ac.uk/user/s19/s1970051/catkin_ws/src/simple_navigation_goals/launch/movebase_seq.launch"
-    launch = roslaunch.parent.ROSLaunchParent(uuid, [path])
-    launch.start()
-    rospy.loginfo("started")
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
+path = "/afs/inf.ed.ac.uk/user/s19/s1970051/catkin_ws/src/simple_navigation_goals/launch/movebase_seq_home.launch"
+
+class Navigation():
+    def __init__(self):
+        print("Initialising Nav")
+        self.action_timeout = 10
+        rospy.init_node('talker', anonymous=True)
+        self.path = "/afs/inf.ed.ac.uk/user/s19/s1970051/catkin_ws/src/simple_navigation_goals/launch/"
+        self.move_base = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        self.move_base.wait_for_server(rospy.Duration(self.action_timeout))
+
+    def launch_file(self, file_name):
+        pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+        rate = rospy.Rate(10) # 10hz
+        uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+        roslaunch.configure_logging(uuid)
+        launch = roslaunch.parent.ROSLaunchParent(uuid, [self.path+file_name])
+        launch.start()
+        rospy.loginfo("started")
+
+
+    def roomA(self):
+        self.launch_file('movebase_seq.launch')
+
+    def home(self):
+        self.launch_file('movebase_seq_home.launch')
+
+    def stop(self):
+        self.move_base.cancel_all_goals()
 
 if __name__ == '__main__':
     try:
