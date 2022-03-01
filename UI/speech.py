2591e7c24822fb1bbe5b8e727714607275ec5ae6
manyconsonants <tzeshuen1999@gmail.com>
2022-03-01 15:49:41 +0000

    Adjusted time before timing out for speech recognition.

--- UI/speech.py
+++ UI/speech.py
@@ -15,23 +15,32 @@ class Speech(sr.Recognizer):
     Using a Microphone for audio input
     """
     def listenMicrophone(self):
+        text = None
         with sr.Microphone() as source:
             # r.adjust_for_ambient_noise(source)
             print("Please say something:")
-            audio = self.listen(source, timeout=3)
+            try:
+                audio = self.listen(source, timeout=0.8)
+                text = self.recognize_google(audio)
+                print("You said: \n" + text)
+            except sr.WaitTimeoutError as e:
+                print(e)
+            except sr.UnknownValueError:
+                print("Google Speech Recognition could not understand audio")
+            except sr.RequestError as e:
+                print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-        # write audio to a WAV file
-        with open("speech.wav", "wb") as f:
-            f.write(audio.get_wav_data())
-
-        text = None
-        try:
-            text = self.recognize_google(audio)
-            print("You said: \n" + text)
-        except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
-        except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        # # write audio to a WAV file
+        # with open("speech.wav", "wb") as f:
+        #     f.write(audio.get_wav_data())
+        #
+        # text = None
+        # try:
+        #
+        # except sr.UnknownValueError:
+        #     print("Google Speech Recognition could not understand audio")
+        # except sr.RequestError as e:
+        #     print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
         return text
 
