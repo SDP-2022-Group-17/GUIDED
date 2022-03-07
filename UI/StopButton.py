b672f50705a7539359991f0e770e355a10cebd78
isaacelliott <isaacelliott99@gmail.com>
2022-03-07 12:26:20 +0000

    further developed StopButton.py
    Signed-off-by: isaacelliott <isaacelliott99@gmail.com>

--- UI/StopButton.py
+++ UI/StopButton.py
@@ -1,8 +1,23 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-while True: # Run forever
-    if GPIO.input(10) == GPIO.HIGH:
-        print("Button was pushed!")
+		
+class StopButton():
+    def __init__(self):
+		GPIO.setwarnings(False) # Ignore warning for now
+		GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+		GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+	
+    def listenButton(self):
+        text = None
+        with BUTTON as source:
+            try:
+				if GPIO.input(10) == GPIO.HIGH:
+					command = 1
+            except Exception as e:
+				print("Error")
+
+        return command
+
+if __name__ == "__main__":
+    buttonpress = StopButton()
+    text = buttonpress.listenButton()
