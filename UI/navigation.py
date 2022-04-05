1383066103311f69076713390e8eccc58d76e847
Marcel Marais <s1970051@cassatt.inf.ed.ac.uk>
2022-04-05 17:56:45 +0100

    Added costmap clearing

--- UI/navigation.py
+++ UI/navigation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import paramiko
 from cryptography.fernet import Fernet
+import time
 
 class Navigation():
     def __init__(self):
@@ -8,7 +9,7 @@ class Navigation():
         fernet = Fernet(key)
         self.password=b'gAAAAABiSwsvHK3dWTHAsuT6R4T_7YdMh5QEArIM3fHmIhQjJugkuLXZLlQtbZjH9_K9XUeCc5y4ptU4xXFtaEDr1e-HDNppoA=='
         self.password = fernet.decrypt(self.password).decode()
-        
+
         self.sdp_robots = 'cassatt'
         self.username = 's1970051'
         self.hostname = 'cassatt.inf.ed.ac.uk'
@@ -26,16 +27,23 @@ class Navigation():
             print("[!] Cannot connect to the SSH Server")
             exit()
 
+    def clear_costmap(self):
+        stdin, stdout, stderr = self.client.exec_command('rosservice call /move_base/clear_costmaps "{}"')
+
     def entrance(self):
+        self.clear_costmap()
         stdin, stdout, stderr = self.client.exec_command('roslaunch simple_navigation_goals entrance.launch')
 
     def office(self):
+        self.clear_costmap()
         stdin, stdout, stderr = self.client.exec_command('roslaunch simple_navigation_goals office.launch')
 
     def restroom(self):
+        self.clear_costmap()
         stdin, stdout, stderr = self.client.exec_command('roslaunch simple_navigation_goals restroom.launch')
 
     def kitchen(self):
+        self.clear_costmap()
         stdin, stdout, stderr = self.client.exec_command('roslaunch simple_navigation_goals kitchen.launch')
 
     def stop(self):
@@ -43,4 +51,5 @@ class Navigation():
 
 if __name__ == '__main__':
     nav = Navigation()
-    nav.office()
+    nav.kitchen()
+    time.sleep(10)
